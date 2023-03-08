126103f671ab8d9132394c1a347bbba94e2826ba
ibrahim badr <ibrahimbdr@outlook.com>
2023-03-08 02:07:08 +0200

    update

--- src/pages/Print.js
+++ src/pages/Print.js
@@ -46,7 +46,7 @@ const Print = () => {
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
   return (
     <div className="fixed w-fit bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2">
       {!showRedirect && showData && (
